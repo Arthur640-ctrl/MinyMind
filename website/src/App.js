3cc5200346ae3d553398543a08325e65ed02ea10
Arthur640-ctrl <arthur.merienne57@gmail.com>
2025-09-18 19:45:01 +0200

    first commit

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur MinyMind 🚀</h1>
+      <p>Le site pour apprendre et expérimenter avec l'IA</p>
+    </div>
+  );
+}
+
+export default App;
